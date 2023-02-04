dbc74385c09c5bdba5bee6b6a0b01ed03604e7dc
Rishabh Gupta <73791635+RGTechPro@users.noreply.github.com>
2023-02-05 01:36:08 +0530

    Initial commit

---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,86 @@
+import 'package:crypto_app/widgets/actions/actions_widget.dart';
+import 'package:crypto_app/widgets/balance_panel/balance_panel.dart';
+import 'package:crypto_app/widgets/chart/chart_home_page.dart';
+import 'package:crypto_app/widgets/type_card.dart';
+import 'package:crypto_font_icons/crypto_font_icons.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:unicons/unicons.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double balance = 66032206.10;
+  double profit = 35.22;
+  double profitPercent = 0.22;
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+    return Scaffold(
+      backgroundColor: themeData.backgroundColor,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(40.0), //appbar size
+        child: AppBar(
+          bottomOpacity: 0.0,
+          elevation: 0.0,
+          shadowColor: Colors.transparent,
+          backgroundColor: themeData.backgroundColor,
+          leading: SizedBox(
+            height: 10.w,
+            width: 15.w,
+          ),
+          automaticallyImplyLeading: false,
+          titleSpacing: 0,
+          leadingWidth: 15.w,
+          title: Text('Finfy'),
+          // title: Image.asset(
+          //   themeData.brightness == Brightness.light
+          //       ? 'assets/sobGOGdark.png'
+          //       : 'assets/sobGOGlight.png',
+          //   height: 3.5.h,
+          // ),
+          centerTitle: true,
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 5.w),
+              child: SizedBox(
+                height: 3.5.h,
+                width: 10.w,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: themeData.primaryColor.withOpacity(0.05),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Icon(
+                    UniconsLine.bell,
+                    color: themeData.primaryColor,
+                    size: 20.sp,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 2.h),
+          child: ListView(
+            children: [
+              balancePanel(balance, profit, profitPercent, themeData),
+              //actionsWidget(themeData),
+              typeCard('Banking'),
+              typeCard('Stock Market'),
+              typeCard('Gold')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/type_card.dart
@@ -0,0 +1,68 @@
+import 'package:crypto_app/pages/details_page.dart';
+import 'package:crypto_app/widgets/balance_panel/profit_percentage.dart';
+import 'package:crypto_app/widgets/chart/chart.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+
+Padding typeCard(
+  String type,
+) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Center(
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: Colors.white.withOpacity(0.2),
+              width: 2,
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 2.h,
+              horizontal: 2.w,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 0.w),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 0.w),
+                        child: SizedBox(
+                          width: 50.w,
+                          height: 10.h,
+                          child: Center(
+                            child: Text(
+                              '$type',
+                              //textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20.sp,
+                                color: Colors.white,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
